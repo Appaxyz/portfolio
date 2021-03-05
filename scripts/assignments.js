3f1cf6d40df7920d7fd35a181509d3170f6000f1
Appaxyz <78294659+Appaxyz@users.noreply.github.com>
2021-03-05 09:58:25 -0500

    Initial commit

---
+++ scripts/assignments.js
@@ -0,0 +1,42 @@
+function fun_1(){
+    alert('Assignment 1 Coming Soon')
+}
+
+function fun_2(){
+    alert('Assignment 2 Coming Soon')
+}
+
+function fun_3(){
+    alert('Assignment 3 Coming Soon')
+}
+
+function fun_4(){
+    alert('Assignment 4 Coming Soon')
+}
+
+function fun_5(){
+    alert('Assignment 5 Coming Soon')
+}
+
+function fun_6(){
+    alert('Assignment 6 Coming Soon')
+}
+
+function fun_7(){
+    alert('Assignment 7 Coming Soon')
+}
+
+function fun_8(){
+    alert('Assignment 8 Coming Soon')
+}
+
+function fun_9(){
+    alert('Assignment 9 Coming Soon')
+}
+
+
+function fun_10(){
+    alert('Assignment 10 Coming Soon')
+}
+
+
